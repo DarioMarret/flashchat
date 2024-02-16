30e8d303ca9a4bffebc6792079601c758cbe2789
DJMarret <javier_dario_marret@hotmail.com>
2024-02-16 13:51:07 -0500

    registrar datos de la cuenta de facebook

--- src/function/util/global.js
+++ src/function/util/global.js
@@ -1,8 +1,7 @@
 export const usuario_local = "usuario:";
 export const usuario_token = "token_usuario:";
-// export const host = "https://api.t-ickets.com/backflash/";
-// export const proxy = "backflash"
-export const host = "http://localhost:5002/";
-export const proxy = ""
+export const host = "https://api.t-ickets.com/backflash/";
+export const proxy = "backflash"
+// export const host = "http:/
 export const host_facturacion = "https://ordenfacil.org/api_facturacion";
 // validar si es produccion o desarrollo
