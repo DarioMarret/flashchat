77dd207f8b3f57389c7c7e57dee6e2bd7f8293f6
DJMarret <javier_dario_marret@hotmail.com>
2024-02-25 14:05:27 -0500

    logica implementada en suscripcion

--- src/views/Pages/Suscripcion/Suscripcion.js
+++ src/views/Pages/Suscripcion/Suscripcion.js
@@ -1,18 +1,44 @@
 import axios from 'axios';
+import { GetTokenDecoded } from 'function/storeUsuario';
 import { host } from 'function/util/global';
 import { useEffect, useState } from 'react';
 import {
   Container,
 } from 'react-bootstrap';
+import Swal from 'sweetalert2';
 // import Swal from 'sweetalert2';
 
 export default function Suscripciones() {
   const [planes, setPlanes] = useState([])
+  const [ListarPlanAsignado, setListarPlanAsignado] = useState(null)
+  const [planSeleccionado, setPlanSeleccionado] = useState({
+    id: 0,
+    cuenta_id: GetTokenDecoded().cuenta_id,
+    plan_id: 0,
+    agente_id: GetTokenDecoded().id,
+    cantidad_agentes: 0,
+    cantidad_bots: 0,
+    precio: 0,
+  })
+  const [planGold, setPlanGold] = useState({
+    id: 0,
+    cuenta_id: GetTokenDecoded().cuenta_id,
+    plan_id: 0,
+    agente_id: GetTokenDecoded().id,
+    cantidad_agentes: 0,
+    cantidad_bots: 0,
+    precio: 0,
+  })
 
   const ListarCuenta = async () => {
     const url = `${host}planes`;
     await axios.get(url)
       .then(response => {
+        response.data.data.map((plan, index) => {
+          if(plan.id === 4){
+            setPlanGold(plan)
+          }
+        })
         setPlanes(response.data.data);
       })
       .catch(error => {
@@ -20,8 +46,70 @@ export default function Suscripciones() {
       })
   }
 
+  const PlanAsignado = async () => {
+    const url = `${host}cuenta_plan/${GetTokenDecoded().cuenta_id}`
+    const { data } = await axios.get(url)
+    console.log(data.data[0])
+    setListarPlanAsignado(data.data[0]);
+  }
+
+  const SeleccionarPlan = (plan) => {
+    if(plan.id === 4){
+      plan = planGold
+    }
+    let info = {
+      ...plan,
+      cuenta_id: GetTokenDecoded().cuenta_id,
+      agente_id: GetTokenDecoded().id
+    }
+    setPlanSeleccionado(info)
+    if(ListarPlanAsignado.plan_id === 1){
+      SuscribirPlan(info)
+      PlanAsignado()
+    }else if(plan.id === 2 || plan.id === 3){
+      Swal.fire({
+        title: '¿Estás seguro?',
+        text: "Al cambiar de plan se perderán los datos de la cuenta",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Cambiar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          SuscribirPlan(info)
+          PlanAsignado()
+        }
+      })
+    }else{
+      SuscribirPlan(info)
+      PlanAsignado()
+    }
+  }
+
+  const SuscribirPlan = async (info) => {
+    const url = `${host}suscribir_plan`
+    await axios.post(url, info)
+    .then(response => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Felicidades',
+        text: 'Plan suscrito con éxito',
+      })
+      PlanAsignado()
+    })
+    .catch(error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Algo salió mal!',
+      })
+    })
+  }
+
   useEffect(() => {
     (async()=>{
+      await PlanAsignado()
       await ListarCuenta()
     })()
   }, []);
@@ -50,33 +138,24 @@ export default function Suscripciones() {
     
                     <div className='text-center d-flex flex-column box-plan-feature-item mt-2'>
                       <p className='m-0'>{plan.cantidad_agentes} Agentes</p>
-                      <p className='m-0'>{plan.cantidad_bots} Canales</p>
+                      <p className='m-0'>{plan.cantidad_bots} Conexiones</p>
                     </div>
                   </div>
     
                   <div className='box-plan-button w-100 mb-3'>
-                    <button className='w-100 box-plan-button-suscribir button-plus'>Suscribir</button>
+                    <button className='w-100 box-plan-button-suscribir button-plus' onClick={()=>SeleccionarPlan(plan)}>Suscribir</button>
                   </div>
     
                   <div className='box-plan-descripcion'>
                     <ul>
-                      <li class="d-flex align-items-center gap-1">
-                        <span class="material-symbols-outlined text-success" 
-                        style={{fontSize: '20px'}}>done</span>
-                        1 GB Almacenamiento multimedia
-                      </li>
-    
-                      <li class="d-flex align-items-center gap-1">
-                        <span class="material-symbols-outlined text-success" 
-                        style={{fontSize: '20px'}}>done</span>
-                        Conversaciones ilimitadas
-                      </li>
-    
-                      <li class="d-flex align-items-center gap-1">
-                        <span class="material-symbols-outlined text-success" 
-                        style={{fontSize: '20px'}}>done</span>
-                        Soporte 24/7
-                      </li>
+                      {
+                        plan.descripcion.map((detall, index) => {
+                            return <li class="d-flex align-items-center gap-1">
+                              <span class="material-symbols-outlined text-success" 
+                              style={{fontSize: '20px'}}>done</span>{detall}
+                            </li>
+                        })
+                      }
                     </ul>
                   </div>
                 </div>
@@ -93,33 +172,24 @@ export default function Suscripciones() {
 
                       <div className='text-center d-flex flex-column box-plan-feature-item mt-2'>
                         <p className='m-0'>{plan.cantidad_agentes} Agentes</p>
-                        <p className='m-0'>{plan.cantidad_bots} Canal</p>
+                        <p className='m-0'>{plan.cantidad_bots} Conexiones</p>
                       </div>
                     </div>
 
                     <div className='box-plan-button w-100 mb-3'>
-                      <button className='w-100 box-plan-button-suscribir'>Suscribir</button>
+                      <button className='w-100 box-plan-button-suscribir' onClick={()=>SeleccionarPlan(plan)}>Suscribir</button>
                     </div>
 
                     <div className='box-plan-descripcion'>
                       <ul>
-                        <li class="d-flex align-items-center gap-1">
-                          <span class="material-symbols-outlined text-success" 
-                          style={{fontSize: '20px'}}>done</span>
-                          1 GB Almacenamiento multimedia
-                        </li>
-
-                        <li class="d-flex align-items-center gap-1">
-                          <span class="material-symbols-outlined text-success" 
-                          style={{fontSize: '20px'}}>done</span>
-                          Conversaciones ilimitadas
-                        </li>
-
-                        <li class="d-flex align-items-center gap-1">
-                          <span class="material-symbols-outlined text-success" 
-                          style={{fontSize: '20px'}}>done</span>
-                          Soporte 24/7
-                        </li>
+                      {
+                        plan.descripcion.map((detall, index) => {
+                            return <li class="d-flex align-items-center gap-1">
+                              <span class="material-symbols-outlined text-success" 
+                              style={{fontSize: '20px'}}>done</span>{detall}
+                            </li>
+                        })
+                      }
                       </ul>
                     </div>
                   </div>
@@ -129,48 +199,61 @@ export default function Suscripciones() {
                     <div className='box-plan-feature d-flex flex-column justify-content-center my-3'>
                       <h2 className='box-plan-feature-price text-blue font-bold text-center'>
                         <sup>$</sup>
-                          {plan.precio}
+                          {planGold.precio}
                         <sub>/mes</sub>
                       </h2>
 
                       <div className='text-center d-flex flex-column box-plan-feature-item mt-2'>
                         <div className='d-flex gap-2 align-items-center justify-content-center'>
-                          <button className="button-operation">-</button>
-                          <p className='m-0'>{plan.cantidad_agentes} Agentes</p>
-                          <button className="button-operation">+</button>
+                          <button className="button-operation"
+                            // no puede ser menos a la cantidad inicial
+                            onClick={()=>{
+                              if(planGold.cantidad_agentes > plan.cantidad_agentes){
+                                setPlanGold({...planGold, cantidad_agentes: planGold.cantidad_agentes - 1, precio: planGold.precio - 10})
+                              }
+                            }}
+                          >-</button>
+                          <p className='m-0'>{planGold.cantidad_agentes} Agentes</p>
+                          <button className="button-operation"
+                            onClick={()=>{
+                              setPlanGold({...planGold, cantidad_agentes: planGold.cantidad_agentes + 1, precio: planGold.precio + 10})
+                            }}
+                          >+</button>
                         </div>
 
                         <div className='d-flex gap-2 align-items-center justify-content-center'>
-                          <button className="button-operation">-</button>
-                          <p className='m-0'>{plan.cantidad_bots} Canales</p>
-                          <button className="button-operation">+</button>
+                          <button className="button-operation"
+                            // no puede ser menos a la cantidad inicial
+                            onClick={()=>{
+                              if(planGold.cantidad_bots > plan.cantidad_bots){
+                                setPlanGold({...planGold, cantidad_bots: planGold.cantidad_bots - 1, precio: planGold.precio - 20})
+                              }
+                            }}
+                          >-</button>
+                          <p className='m-0'>{planGold.cantidad_bots} Conexiones</p>
+                          <button className="button-operation"
+                            onClick={()=>{
+                              setPlanGold({...planGold, cantidad_bots: planGold.cantidad_bots + 1, precio: planGold.precio + 20})
+                            }}
+                          >+</button>
                         </div>
                       </div>
                     </div>
 
                     <div className='box-plan-button w-100 mb-3'>
-                      <button className='w-100 box-plan-button-suscribir'>Suscribir</button>
+                      <button className='w-100 box-plan-button-suscribir' onClick={()=>SeleccionarPlan(plan)}>Suscribir</button>
                     </div>
 
                     <div className='box-plan-descripcion'>
                       <ul>
-                        <li class="d-flex align-items-center gap-1">
-                          <span class="material-symbols-outlined text-success" 
-                          style={{fontSize: '20px'}}>done</span>
-                          1 GB Almacenamiento multimedia
-                        </li>
-
-                        <li class="d-flex align-items-center gap-1">
-                          <span class="material-symbols-outlined text-success" 
-                          style={{fontSize: '20px'}}>done</span>
-                          Conversaciones ilimitadas
-                        </li>
-
-                        <li class="d-flex align-items-center gap-1">
-                          <span class="material-symbols-outlined text-success" 
-                          style={{fontSize: '20px'}}>done</span>
-                          Soporte 24/7
-                        </li>
+                      {
+                        plan.descripcion.map((detall, index) => {
+                            return <li class="d-flex align-items-center gap-1">
+                              <span class="material-symbols-outlined text-success" 
+                              style={{fontSize: '20px'}}>done</span>{detall}
+                            </li>
+                        })
+                      }
                       </ul>
                     </div>
                   </div>
