cc2e9866c9240a212d74bb3cf2267a79af0d9c63
Sergio Floreano Tomalá <46009036+sergio-roger@users.noreply.github.com>
2024-02-24 22:59:31 -0500

    Diseño de view 'Suscripciones'

--- src/views/Pages/Factura/Factura.js
+++ src/views/Pages/Factura/Factura.js
@@ -1,6 +1,22 @@
+import { Container, Tab, Tabs } from "react-bootstrap";
 
 export default function Factura() {
   return (
-    <div>Factura</div>
+    <>
+      <Container fluid>
+        <Tabs 
+          defaultActiveKey="Historial" 
+          id="uncontrolled-tab-example"
+          className='justify-content-center'
+          >
+          <Tab eventKey="Historial" title="Historial">
+            <h2>Historial</h2>
+          </Tab>
+          <Tab eventKey="Facturas" title="Facturas">
+            <h2>Facturas</h2>
+          </Tab>
+        </Tabs>
+      </Container>
+    </>
   )
 }
