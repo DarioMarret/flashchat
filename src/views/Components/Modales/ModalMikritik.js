dfd12f174043a7d90b1b58f5b6cb006c0914117b
DJMarret <javier_dario_marret@hotmail.com>
2024-03-17 09:20:06 -0500

    Session de Factura

--- src/views/Components/Modales/ModalMikritik.js
+++ src/views/Components/Modales/ModalMikritik.js
@@ -1,5 +1,7 @@
 
-import { colorPrimario } from "function/util/global";
+import { GetTokenDecoded } from "function/storeUsuario";
+import { BmHttp, colorPrimario } from "function/util/global";
+import { useState } from "react";
 import {
     Col,
     Modal,
@@ -7,14 +9,83 @@ import {
 } from "react-bootstrap";
 
 function ModalMikritik(props) {
-    const { showMidkritik, onHideMidkritik, bots, estados } = props;
+    const { showMidkritik, onHideMidkritik, bots } = props;
+    const [infoMidkritik, setInfoMidkritik] = useState({
+        id: "",
+        conexion: "",
+        host: "",
+        cuenta_id: GetTokenDecoded().cuenta_id,
+        nombreunico: "",
+        token: "",
+        dp: "",
+        variable: "",
+        acciones: "CrearTicketMikrotik"
+    })
+
+    const Limpiar = () => {
+        setInfoMidkritik({
+            conexion: "",
+            host: "",
+            cuenta_id: GetTokenDecoded().cuenta_id,
+            nombreunico: "",
+            token: "",
+            dp: "",
+            variable: "",
+            acciones: "CrearTicketMikrotik"
+        })
+    }
+
+    const handleChange = (e) => {
+        setInfoMidkritik({
+            ...infoMidkritik,
+            [e.target.name]: e.target.value
+        })
+        if(e.target.name === "conexion" && e.target.value !== ""){
+            ListarInfoMikritik(e.target.value)
+        }
+    }
+
+    const GuardarMidkritik = async() => {
+        try {
+            const { data, status } = await BmHttp.post("crearte_mikrotik", infoMidkritik)
+            if(data.status === 200){
+                showMidkritik()
+                Limpiar()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const ListarInfoMikritik = async (nombreunico) => {
+        try {
+            const { data, status } = await BmHttp.get("obtener_mikrotik/"+nombreunico)
+            if(data.status === 200){
+                setInfoMidkritik({
+                    id: data.data.id,
+                    conexion: data.data.conexion,
+                    host: data.data.host,
+                    cuenta_id: data.data.cuenta_id,
+                    nombreunico: data.data.nombreunico,
+                    token: data.data.token,
+                    dp: data.data.dp,
+                    variable: data.data.variable,
+                    acciones: data.data.acciones
+                })
+            }else{
+                Limpiar()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
     return (
         <Modal
             show={onHideMidkritik}
             onHide={showMidkritik}
             size="lg"
-            // aria-labelledby="contained-modal-title-vcenter"
-            // centered
         >
             <Modal.Header>
             <div className="d-flex justify-content-between w-100">
@@ -40,6 +111,8 @@ function ModalMikritik(props) {
                     <label>Conexion</label>
                     <select
                         className="form-control"
+                        name="conexion"
+                        onChange={(e)=>handleChange(e)}
                     >
                         <option value="">Seleccione</option>
                         {
@@ -57,6 +130,9 @@ function ModalMikritik(props) {
                         type="text"
                         className="form-control"
                         placeholder="Host"
+                        name="host"
+                        onChange={(e)=>handleChange(e)}
+                        value={infoMidkritik.host}
                     />
                 </div>
                 <div className="form-group">
@@ -65,6 +141,9 @@ function ModalMikritik(props) {
                         type="text"
                         className="form-control"
                         placeholder="Token"
+                        name="token"
+                        onChange={(e)=>handleChange(e)}
+                        value={infoMidkritik.token}
                     />
                 </div>
                 <div className="form-group">
@@ -73,70 +152,36 @@ function ModalMikritik(props) {
                 <Row>
                     <Col>
                         <div className="form-group">
-                            <label>Estado</label>
-                            <select
+                            <label>ID Departamento mikroti</label>
+                            <input
+                                type="number"
                                 className="form-control"
-                            >
-                                <option value="">Seleccione</option>
-                                {
-                                    estados.map((estado, index) => {
-                                        return (
-                                            <option key={index} value={estado.estados}>{estado.estados}</option>
-                                        )
-                                    })
-                                }
-                            </select>
+                                name="dp"
+                                placeholder="ID Departamento"
+                                onChange={(e)=>handleChange(e)}
+                                value={infoMidkritik.dp}
+                            />
                         </div>
                     </Col>
                     <Col>
                         <div className="form-group">
-                            <label>Acción</label>
-                            <select
+                            <label>Nombre de variable en el flujo del bot</label>
+                            <input
+                                type="text"
                                 className="form-control"
-                            >
-                                <option value="">Seleccione</option>
-                                <option value="1">Activar</option>
-                                <option value="0">Desactivar</option>
-                            </select>
+                                name="variable"
+                                placeholder="ClienteCedula"
+                                onChange={(e)=>handleChange(e)}
+                                value={infoMidkritik.variable}
+                            />
                         </div>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
-                        <div className="form-group">
-                            <label>Estado</label>
-                            <select
-                                className="form-control"
-                            >
-                                <option value="">Seleccione</option>
-                                {
-                                    estados.map((estado, index) => {
-                                        return (
-                                            <option key={index} value={estado.estados}>{estado.estados}</option>
-                                        )
-                                    })
-                                }
-                            </select>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="form-group">
-                            <label>Acción</label>
-                            <select
-                                className="form-control"
-                            >
-                                <option value="">Seleccione</option>
-                                <option value="1">Activar</option>
-                                <option value="0">Desactivar</option>
-                            </select>
-                        </div>
-                    </Col>
-                </Row>                
             </Modal.Body>
             <Modal.Footer>
                 <button
                     className="btn btn button-bm w-100"
-                    onClick={showMidkritik}
+                    onClick={()=>GuardarMidkritik()}
                 >
                     Guardar
                 </button>
