12b892dce51e4432384bc9292695c0e7d6ba19d6
Sergio Floreano Tomalá <46009036+sergio-roger@users.noreply.github.com>
2024-03-23 20:03:59 -0500

    Parametrizar alerta

--- src/views/Components/Alert/Alert.js
+++ src/views/Components/Alert/Alert.js
@@ -1,17 +1,33 @@
 function AlertBanner(props) {
-  const { message } = props;
+  const { message, type, btnColor } = props;
   
+  let alertColor;
+
+  switch (type) {
+    case 'warning':
+      alertColor = 'bg-warning';
+      break;
+    case 'info':
+      alertColor = 'bg-info';
+      break;
+    case 'danger':
+      alertColor = 'bg-danger';
+      break;
+    default:
+      alertColor = 'bg-info';
+  }
+
   return(
   <>
     <div className="w-100 p-3">
-      <section className="border bg-danger mx-auto p-2 px-3 rounded d-flex justify-content-between align-items-center"  
+      <section className={`${alertColor} mx-auto p-2 px-3 rounded d-flex justify-content-between align-items-center`}  
         style={{ width: "90%" }}>
         <h5 className="text-center font-bold text-white mt-1" 
           style={{ 'fontSize': '16px' }}>
           {message}
         </h5>
 
-        <button className="btn btn-danger">X</button>
+        <button className={`btn ${btnColor}`}>X</button>
       </section>
     </div>
   </>);
